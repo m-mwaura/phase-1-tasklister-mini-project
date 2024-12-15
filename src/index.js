19b535361398842a8c7c247357b1ad912457475b
felix mwangi <fmwauramwangi@gmail.com>
2024-12-15 09:07:16 +0300

    src/index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+const newTaskForm = document.querySelector('form');
+taskForm.addEventListener('submit', (events) => {
+  events.preventDefault();
+
+  const newToDo = events.target.querySelector('#task-description').value
+  buildToDo(newToDo);
+  newTaskForm.reset();
+})
+
 });
+
+function buildToDo(newToDo) {
+  const taskLi = document.createElement('li');
+  const deleteButton = document.createElement('button');
+
+deleteButton.textContent = 'x';
+taskLi.appendChild(deleteButton);
+document.querySelector('#tasks').appendChild(taskLi);
+deleteButton.addEventListener('click', handleDelete);
+
+}
+
+function handleDelete (events) {
+  events.target.parentNode.remove();
+}
